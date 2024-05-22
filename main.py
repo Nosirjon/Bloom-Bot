d6813ce28f913da2a5496597bc6f63bcdd5b5f6b
nosir <jon95xa@gmail.com>
2024-05-22 08:36:45 +0500

    add file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('6939780440:AAER7H3mcLR3e0oTlIW6OXc0ElmnlMiOpJ0')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     markup.add(types.InlineKeyboardButton(text='Посетить сайт', web_app=types.WebAppInfo(url='https://gasoil.uz')))
     bot.send_message(message.chat.id, text='Здравствуйте я бот компании Bloom, бот пока же то на разработке вы можите посетить наш сайт нажав кнопку посетить сайт', reply_markup=markup)
 
